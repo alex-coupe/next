b121850b1b03396f5719f246bce5ae2825f99729
Alex <acooupe@gmail.com>
2021-02-13 10:50:57 +0000

    Redirecting

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import {useEffect} from 'react';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 
 function NotFound(props) {
+
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/");
+        }, 3000);
+    }, []);
+
     return (
         <div className="not-found">
             <h1>Oooops.....</h1>
