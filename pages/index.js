08324d724ec59ce53769c9c0637e53a9b738faae
Alex <acooupe@gmail.com>
2021-02-13 10:17:14 +0000

    Layout component

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-    <NavBar/>
     <h1>Home</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu lobortis elementum nibh tellus. Proin fermentum leo vel orci. Urna duis convallis convallis tellus id interdum velit. Et tortor at risus viverra adipiscing. Quam viverra orci sagittis eu volutpat odio facilisis. Suspendisse potenti nullam ac tortor. Amet nisl suscipit adipiscing bibendum est ultricies</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu lobortis elementum nibh tellus. Proin fermentum leo vel orci. Urna duis convallis convallis tellus id interdum velit. Et tortor at risus viverra adipiscing. Quam viverra orci sagittis eu volutpat odio facilisis. Suspendisse potenti nullam ac tortor. Amet nisl suscipit adipiscing bibendum est ultricies</p>
-    <Footer/>
     </div>
   )
 }
