30bdb4efef8c35d3ed39c2edc4b4491caeebd57b
Alex <acooupe@gmail.com>
2021-02-13 12:07:50 +0000

    Dynamic routes

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -1,26 +1,57 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-/*export const getStaticPaths = async () => {
+
+
+export const getStaticPaths = async () => {
     const response = await fetch('https://fakestoreapi.com/products/categories');
     const data = await response.json();
    
 
     const paths = data.map(category => {
         category = category.replace(/\s+/g, '-').toLowerCase();
-        console.log(category);
+        
         return {
             params:{
                slug: category
             }
         }
-    })
+    });
 
+   
     return {
         paths,
         fallback: false
     }
-}*/
+}
+
+export const getStaticProps = async (context) => {
+
+  const items =  [
+    {
+      slug: "men-clothing",
+      text: "Men's Clothing"
+    },
+    {
+      slug: "women-clothing",
+      text: "Women's Clothing"
+    },
+    {
+      slug: "electronics",
+      text: "electronics"
+    },
+    {
+      slug: "jewelery",
+      text: "jewellery"
+    }
+  ]
+
+  const category = items.find(item => item.slug == context.params.slug);
+
+  return{ 
+    props :  category
+  }
+}
 
 function categories(props) {
    
--- pages/categories/index.js
+++ pages/categories/index.js
@@ -3,31 +3,37 @@ import React from 'react';
 import styles from '../../styles/Categories.module.css';
 import Head from 'next/head';
 
-function index(props) {
+export const getStaticProps = async () => {
+    const response = await fetch('https://fakestoreapi.com/products/categories');
+    const data = await response.json();
+
+    return {
+        props:{
+            categories:data
+        }
+    }
+}
+
+function index({categories}) {
+  
     return (
         <>
         <Head>
         <title>Fake Shop | Categories</title>
-        
         </Head>
         <div className={styles.flexcontainer}>
-            <div className={styles.card}>
-                <div className={styles.container}>
-                <Link href="/categories/electronics"><a><h4><b>Electronics</b></h4></a></Link>
-                </div>
-            </div> 
+        {categories.map(category => {
+            return (
+                <div className={styles.card} key={category}>
+                    <div className={styles.container}>
+                        <Link href={`/categories/${category.replace(/\s+/g, '-').toLowerCase()}`}><a><h4><b>{category}</b></h4></a></Link>
+                    </div>
+                </div> 
+            )
+        })}
 
-            <div className={styles.card}>
-                <div className={styles.container}>
-                   <Link href="/categories/women-clothing"><a><h4><b>Women's Clothes</b></h4></a></Link>
-                </div>
-            </div> 
+        
 
-            <div className={styles.card}>
-                <div className={styles.container}>
-                <Link href="/categories/men-clothing"><a><h4><b>Men's Clothes</b></h4></a></Link>
-                </div>
-            </div> 
 
         </div>
         </>
