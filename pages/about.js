80d810acec847e866e84d264a4e799b5968052da
Alex <acooupe@gmail.com>
2021-02-13 09:29:57 +0000

    Routing and pages

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function about(props) {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu lobortis elementum nibh tellus. Proin fermentum leo vel orci. Urna duis convallis convallis tellus id interdum velit. Et tortor at risus viverra adipiscing. Quam viverra orci sagittis eu volutpat odio facilisis. Suspendisse potenti nullam ac tortor. Amet nisl suscipit adipiscing bibendum est ultricies</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu lobortis elementum nibh tellus. Proin fermentum leo vel orci. Urna duis convallis convallis tellus id interdum velit. Et tortor at risus viverra adipiscing. Quam viverra orci sagittis eu volutpat odio facilisis. Suspendisse potenti nullam ac tortor. Amet nisl suscipit adipiscing bibendum est ultricies</p>
+    
+        </div>
+    );
+}
+
+export default about;   
