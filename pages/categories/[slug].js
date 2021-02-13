c43c93b016a0772137c1fc346fac907e163acdf2
Alex <acooupe@gmail.com>
2021-02-13 12:29:42 +0000

    Category paths mapped with dynamic product loading based on category

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useRouter } from 'next/router';
-
+import styles from '../../styles/Categories.module.css';
+import Image from 'next/image';
 
 
 export const getStaticPaths = async () => {
@@ -42,22 +42,41 @@ export const getStaticProps = async (context) => {
     },
     {
       slug: "jewelery",
-      text: "jewellery"
+      text: "jewelery"
     }
   ]
 
   const category = items.find(item => item.slug == context.params.slug);
 
-  return{ 
-    props :  category
+  const res = await fetch(`https://fakestoreapi.com/products/category/${category.slug}`)
+  const data = await res.json();
+  console.log(data);
+
+  return {
+    props: {category, data}
   }
+
 }
 
-function categories(props) {
+function categories({category, data}) {
    
     return (
         <div>
-            <h1>Category </h1>
+            <h1>{category.text}</h1>
+            {data.map((item) => {
+              return (
+              <div className={styles.card} key={item.id}>
+                  <Image src={item.image} width={300} height={300} alt={item.title}/>
+                  <div className={styles.container}>
+                  <a><h4><b>{item.title}</b></h4></a>
+                  <ul>
+                    <li>{item.description}</li>
+                    <li><b>{item.price}</b></li>
+                  </ul>
+                  <div className="btn"><a href="/">Buy Now</a></div>
+                </div>
+            </div> )
+            })}
         </div>
     );
 }
